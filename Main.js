749ce90497a0f287b693711597ad6784f964598d
itzzyaboyterr <itzzyaboyterr@protonmail.com>
2023-12-06 13:43:49 +0100

    Remove bloat

--- Main.js
+++ Main.js
@@ -5,31 +5,26 @@ JSBKC.keys = {};
 JSBKC.keys.up = null;
 JSBKC.keys.down = null;
 
-function Runtime() {
-    //Check if key down
-    document.addEventListener('keydown', Downcheck);
+//Check if key down
+document.addEventListener('keydown', Downcheck);
 
-    //Check if key up
-    document.addEventListener('keyup', Upcheck);
+//Check if key up
+document.addEventListener('keyup', Upcheck);
 
-    function Downcheck() {
-        if(event.code !== JSBKC.keys.down) {
-            JSBKC.keys.up = null;
-            JSBKC.keys.down = event.code;
-        }
-        document.removeEventListener('keydown', Downcheck);
-    }
-
-    function Upcheck() {
-        if(event.code !== JSBKC.keys.up) {
-            JSBKC.keys.down = null;
-            JSBKC.keys.up = event.code;
-        }
-        document.removeEventListener('keyup', Upcheck);
+function Downcheck() {
+    if(event.code !== JSBKC.keys.down) {
+        JSBKC.keys.up = null;
+        JSBKC.keys.down = event.code;
     }
 }
 
-setInterval(Runtime, 25);
+function Upcheck() {
+    if(event.code !== JSBKC.keys.up) {
+        JSBKC.keys.down = null;
+        JSBKC.keys.up = event.code;
+    }
+    document.removeEventListener('keyup', Upcheck);
+}
 //functions
 function IsKeyDown(key) {
     if(JSBKC.keys.down == key) {
